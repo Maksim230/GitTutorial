6278a660417b7a1ac805fe71315026a783028933
Maksim Lazutkin <lazutkin.maksim230@mail.ru>
2017-09-08 13:44:14 +0400

    add Programma.cs

---
+++ Programa.cs
@@ -0,0 +1,22 @@
+class Person
+{
+   public int X;
+   public int Y; 
+   public Person(int x,int y)
+   {
+       this.X = x;
+       this.Y = y;
+   }
+}
+using system;
+namespace class
+{
+  void Main()
+{
+Console.Write("Hello World");
+    Console.ReadKey();
+}
+Console.ReadKey();
+Богдан Умный
+Bogdan
+Maksim
